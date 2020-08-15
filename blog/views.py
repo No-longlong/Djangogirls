5d1ebe4c260ea4da4123eaca0bee75b74b2e9e22
longTimeNoSee <johnberboy@naver.com>
2020-08-15 13:41:12 +0900

    Query set

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 # render는 장고가 지원해주는 템플릿
-def post_list(request):
-    return render(request, 'blog/post_list.html')
+
+def post_list(request): # 뷰(함수)에서 준비해서, 템블릿에 넘기는(return) 코드
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now()) # 현재시간보다 같거나 작은
+    qs = qs.order_by('published_date') 
+    
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
